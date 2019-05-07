1778e0e8aeadab80b4cb75bf154a59ca9021deff
Berni <berni@swissonline.ch>
2019-05-07 18:58:38 +0200

    add pipe_90

--- app/src/main/java/bertrand/myopengl/ExampleScenes/Test_3.java
+++ app/src/main/java/bertrand/myopengl/ExampleScenes/Test_3.java
@@ -48,30 +48,15 @@ public final class Test_3 {
                 );
 
                 ModelData obj = OBJParser.transform(
-                        asset.open("LowPoly_Islands/waterTower.obj")
+                        asset.open("LowPoly_Islands/pipe_90.obj")
                 );
 
                 Bitmap bitmap = BitmapFactory.decodeStream(
-                        asset.open("LowPoly_Islands/waterTower.png")
+                        asset.open("LowPoly_Islands/pipe_90.png")
                 );
 
                 final Vec3 offset = Positions.offset(obj.getVertices());
-                /*
-                final float[] translationMatrix = new float[16];
-                Matrix.setIdentityM(translationMatrix, 0);
-                Matrix.rotateM(translationMatrix,0, 90, 1, 0, 0);
-                Matrix.rotateM(translationMatrix,0, 90, 0, 1, 0);
-                Matrix.rotateM(translationMatrix,0, 0,  0, 0, 1);
-                final float[] normals = Positions.multiplyMatrix(
-                        translationMatrix,
-                        obj.getNormals()
-                );
-                Matrix.translateM(translationMatrix,0, -offset.x, -offset.y, -offset.z);
-                final float[] positions = Positions.multiplyMatrix(
-                        translationMatrix,
-                        obj.getVertices()
-                );
-                */
+
                 int mesh_ID = Load.texturedModel(
                         Box.meshes,
                         bitmap,
